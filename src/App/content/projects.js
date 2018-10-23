72aa0bf594e2afc35d6a3192d533eb351c029fe3
Klint <kmane@airfind.com>
2018-10-23 09:28:59 +0200

    Fix project links, fix test post typo

--- src/App/content/projects.js
+++ src/App/content/projects.js
@@ -2,16 +2,16 @@ export default [
   {
     title: "Ape",
     description: "A dynamic, general-purpose programming language",
-    link: "https://github.com/klintmane/ape"
+    link: "//ape-lang.github.io"
   },
   {
     title: "Trails",
     description: "A simple, context-based HTTP router implementation in Go",
-    link: "https://github.com/klintmane/trails"
+    link: "//github.com/klintmane/trails"
   },
   {
     title: "Redux Modulist",
     description: "Modularizes the architecture of your Redux applications",
-    link: "https://github.com/turningspace/redux-modulist"
+    link: "//github.com/turningspace/redux-modulist"
   }
 ];
